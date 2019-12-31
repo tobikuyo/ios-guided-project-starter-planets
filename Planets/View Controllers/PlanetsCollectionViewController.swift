da3438bc11631c82daab13ef8aba5b7f0bfcf712
Tobi Tselios <tobi.tselios@gmail.com>
2019-12-31 14:59:55 +0000

    Collection view controller set up

--- Planets/View Controllers/PlanetsCollectionViewController.swift
+++ Planets/View Controllers/PlanetsCollectionViewController.swift
@@ -8,50 +8,27 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
 
 class PlanetsCollectionViewController: UICollectionViewController {
+    
+    // MARK: - Properties
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
+    let planetController = PlanetController()
 
     // MARK: UICollectionViewDataSource
 
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        return planetController.planets.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PlanetCell", for: indexPath) as? PlanetCollectionViewCell else { return UICollectionViewCell() }
+        
+        let planet = planetController.planets[indexPath.item]
+        
+        cell.imageView.image = planet.image
+        cell.textLabel.text = planet.name
+        
         return cell
     }
 
---
+++ Planets/Views/PlanetCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetCollectionViewCell.swift
+//  Planets
+//
+//  Created by Tobi Kuyoro on 31/12/2019.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class PlanetCollectionViewCell: UICollectionViewCell {
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
+    
+}
